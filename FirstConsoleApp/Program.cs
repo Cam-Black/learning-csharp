9f130cf47203180f9e5bcef0b04b1b4ae60ad78e
Cam-Black <cameronablack1998@gmail.com>
2022-09-28 16:49:28 +0100

    Changed config to use .NET 5.0 implementation, disabling implicit usings.

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿class FirstConsoleApp
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
