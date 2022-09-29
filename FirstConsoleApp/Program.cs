03bb907c35c6c4b19d98eb8cbcddcbd7440f7e28
Cam-Black <cameronablack1998@gmail.com>
2022-09-29 09:00:36 +0100

    Added namespace.

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,11 +1,14 @@
-﻿class FirstConsoleApp
+﻿namespace FirstConsoleApp
 {
-    public static void Main(string[] args)
+    class FirstConsoleApp
     {
-        string name = args[0];
-        string location = args[1];
-        int age = int.Parse(args[2]);
-        System.Console.WriteLine("Hello " + name + " from " + location + ".");
-        System.Console.WriteLine("You are " + age + " years old");
+        public static void Main(string[] args)
+        {
+            string name = args[0];
+            string location = args[1];
+            int age = int.Parse(args[2]);
+            System.Console.WriteLine("Hello " + name + " from " + location + ".");
+            System.Console.WriteLine("You are " + age + " years old");
+        }
     }
 }
