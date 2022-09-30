5e193f242cd9cd3979a1b34844b756ee3abc1769
Cam-Black <cameronablack1998@gmail.com>
2022-09-30 12:32:49 +0100

    Added property methods for FirstName, Surname and DoB.

--- Inheritance/Inheritance/Person/Person.cs
+++ Inheritance/Inheritance/Person/Person.cs
@@ -2,5 +2,16 @@
 
 public class Person
 {
-    
+    public Person() {}
+
+    public Person(string firstName, string surname, string dob)
+    {
+        FirstName = firstName;
+        Surname = surname;
+        DoB = DateOnly.Parse(dob);
+    }
+
+    public string FirstName { get; set; } = "Jon";
+    public string Surname { get; set; } = "Doe";
+    public DateOnly DoB { get; set; } = new DateOnly(2000, 01, 01);
 }
