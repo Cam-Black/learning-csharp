3fd943bc4309740fe22716017f7ac9b3419737a3
Cam-Black <cameronablack1998@gmail.com>
2022-09-30 12:15:18 +0100

    Created functionality to create a new Student object with nothing or everything assigned on instantiation. Used auto-properties to set and get values. Refactored Solution folder name to ObjectPractice

---
+++ Student/ObjectPractice/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Student;
+
+public class Student
+{
+    public Student() {}
+    public Student(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+    public string FirstName { get; set; } = "Jon";
+    public string LastName { get; set; } = "Doe";
+    public int Age { get; set; } = 10;
+
+    public override string ToString()
+    {
+        return $"firstName: {FirstName}, lastName: {LastName}, Age: {Age}";
+    }
+}
--- Student/Student/Student.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Student;
-
-public class Student
-{
-    private string firstName;
-    private string lastName;
-    private int age;
-}
