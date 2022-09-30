42b323b02a753e05098a7367dba451e4e1af092f
Cam-Black <cameronablack1998@gmail.com>
2022-09-30 12:15:54 +0100

    Tested cases of Student objects, getting and setting values when instantiating and afterwards.

---
+++ Student/ObjectPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Student
+{
+    internal static class Runner
+    {
+        public static void Main(string[] args)
+        {
+            Student jon = new();
+            Student ace = new Student("Ace", "Kokuren", System.Int32.MaxValue);
+            System.Console.WriteLine(jon);
+            jon.FirstName = "Steve";
+            System.Console.WriteLine(jon);
+            System.Console.WriteLine(ace);
+            ace.Age++;
+            System.Console.WriteLine(ace.Age);
+            System.Console.ReadLine();
+        }
+    }
+}
