18d6eac58a8056e3e6bb177016822e23f990acf1
Cam-Black <cameronablack1998@gmail.com>
2022-09-30 10:50:50 +0100

    Moved git repo to root of Learning CSharp directory.

---
+++ FirstConsoleApp/FirstConsoleApp/Array.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstConsoleApp
+{
+    public class IntArray
+    {
+        private readonly int[] _arr;
+
+        public IntArray(int el)
+        {
+            _arr = new int[el];
+        }
+        
+        public int[] PopIntArr()
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                _arr[i] = i;
+            }
+
+            return _arr;
+        }
+
+        public void PrintIntArr()
+        {
+            foreach (int i in _arr)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstConsoleApp
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            FirstConsoleApp.IntArray myArr = new IntArray(5);
+            myArr.PopIntArr();
+            myArr.PrintIntArr();
+
+        }
+    }
+}
