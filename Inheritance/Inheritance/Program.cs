754928b9f5e9dc004ecc3bd663ca1b3bbb50192e
Cam-Black <cameronablack1998@gmail.com>
2022-09-30 12:52:47 +0100

    Instantiated a new person object, printing out the default values, then used user input to re-assign the values.

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Inheritance.Person.Person person = new();
-Console.WriteLine($"Hello, {person}!");
+System.Console.WriteLine(person.ToString());
+
+System.Console.WriteLine("Enter first name: ");
+string? firstName = System.Console.ReadLine();
+System.Console.WriteLine("Enter last name: ");
+string? lastName = System.Console.ReadLine();
+System.Console.WriteLine("Enter dob format yyyy-mm-dd: ");
+string? dob = System.Console.ReadLine();
+
+person.FirstName = firstName;
+person.Surname = lastName;
+person.DoB = System.DateOnly.Parse(dob);
+System.Console.WriteLine($"Hello, {person.FirstName} {person.Surname}! ");
+System.Console.WriteLine($"You were born {person.DoB}");
